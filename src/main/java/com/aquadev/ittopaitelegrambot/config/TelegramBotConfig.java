ad5aaf6bbb4149cfdb27be590ef9c095aebd281b
Ворончихин Иван <aqua.dev@yandex.com>
2026-03-15 16:58:45 +0300

    Simplify TelegramClient bean configuration

--- src/main/java/com/aquadev/ittopaitelegrambot/config/TelegramBotConfig.java
+++ src/main/java/com/aquadev/ittopaitelegrambot/config/TelegramBotConfig.java
@@ -1,47 +1,20 @@
 package com.aquadev.ittopaitelegrambot.config;
 
-import com.aquadev.ittopaitelegrambot.config.properties.ProxyProperties;
 import com.aquadev.ittopaitelegrambot.config.properties.TelegramProperties;
 import lombok.RequiredArgsConstructor;
-import okhttp3.ConnectionPool;
-import okhttp3.Dispatcher;
-import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
-
-import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.util.concurrent.TimeUnit;
 
 @Configuration
 @RequiredArgsConstructor
 public class TelegramBotConfig {
 
     private final TelegramProperties telegramProperties;
-    private final ProxyProperties proxyProperties;
 
     @Bean
     public TelegramClient telegramClient() {
-        Dispatcher dispatcher = new Dispatcher();
-        dispatcher.setMaxRequestsPerHost(20);
-        dispatcher.setMaxRequests(40);
-
-        OkHttpClient.Builder builder = new OkHttpClient.Builder()
-                .dispatcher(dispatcher)
-                .connectionPool(new ConnectionPool(20, 55, TimeUnit.SECONDS))
-                .connectTimeout(5, TimeUnit.SECONDS)
-                .readTimeout(10, TimeUnit.SECONDS)
-                .writeTimeout(10, TimeUnit.SECONDS);
-
-        if (proxyProperties.isEnabled()) {
-            builder.proxy(new Proxy(
-                    Proxy.Type.SOCKS,
-                    new InetSocketAddress(proxyProperties.host(), proxyProperties.port())
-            ));
-        }
-
-        return new OkHttpTelegramClient(builder.build(), telegramProperties.token());
+        return new OkHttpTelegramClient(telegramProperties.token());
     }
 }
